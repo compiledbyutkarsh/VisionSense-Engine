48bc58aac9e63d18f752a83e8422cd51a8847675
Utkarsh Sachan <utkarshsachan2022@gmail.com>
2026-05-23 18:59:00 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -15,18 +15,18 @@ while cap.isOpened():
         print("Camera access nahi mil raha.")
         break
 
-    # MediaPipe RGB image mangta hai
+    
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = face_detection.process(image_rgb)
 
-    # Face detect hone par box banao
+    
     if results.detections:
         for detection in results.detections:
             bboxC = detection.location_data.relative_bounding_box
             ih, iw, ic = image.shape
             bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
                    int(bboxC.width * iw), int(bboxC.height * ih)
-            # Green box face ke charo taraf
+            
             cv2.rectangle(image, bbox, (0, 255, 0), 3)
 
     cv2.imshow('Utkarsh ka MediaPipe Detector', image)
